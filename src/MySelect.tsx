a2e8afd227bd7b4dd3e72c79f8a4cc58ca77a7d8
Freewind <nowindlee@gmail.com>
2022-04-18 19:53:11 +0800

    fix long option

--- src/MySelect.tsx
+++ src/MySelect.tsx
@@ -1,11 +1,10 @@
 import {Select} from 'antd';
 import React from 'react'
 
-import './MySelect.css';
 
 export default function MySelect() {
   const users = ['aaa', 'bbb', 'ccc cc cccc cc cccc', 'ddd']
-  return <Select className='hello' dropdownClassName={'wrapped-select-option'} placeholder={'User'}>
+  return <Select className='hello' dropdownClassName={'wrapped-select-option'} placeholder={'User'} dropdownMatchSelectWidth={false}>
     {users.map(user => <Select.Option key={user} value={user}>{user}</Select.Option>)}
   </Select>
 };
